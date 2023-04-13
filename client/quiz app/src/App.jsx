3dbac718c290f534987d5cad59a2727b3c5d8cc9
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-04-13 20:16:27 +0100

    update

--- client/quiz app/src/App.jsx
+++ client/quiz app/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Landing from "./pages/Landing"
+import Login from "./pages/LoginForm"
+import Register from "./pages/SignupForm"
+import Dashboard from "./pages/Dashboard"
+import Header from "./components/Header"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ProtectedRoute } from "./components/ProtectedRoute"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+
+      <Router>
+        {/* <div className="flex justify-center items-center py-8">
+          <Header />
+        </div> */}
+
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            {/* <Route path='/dashboard' element={<Dashboard />} /> */}
+          </Routes>
+        </div>
+      </Router>
+
+
+    </>
+
   )
 }
 
